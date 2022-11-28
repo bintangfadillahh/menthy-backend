09db6fbcac88e976c3988c9d9827e6641ac2f4bf
star2112 <bintangfadel73@gmail.com>
2022-11-28 20:47:49 +0700

    fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const cors = require("cors");
 var cookieParser = require("cookie-parser");
 const db = require("./config/database");
 const allRoutes = require("./routes");
